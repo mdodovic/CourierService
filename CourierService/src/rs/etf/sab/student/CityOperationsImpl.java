7c9d229476f302f8f44b9c096f2d4f86f9fd8a98
mdodovic <matija.dodovic@gmail.com>
2022-06-01 12:36:54 +0200

    Delete City by Id and by Name

--- CourierService/src/rs/etf/sab/student/CityOperationsImpl.java
+++ CourierService/src/rs/etf/sab/student/CityOperationsImpl.java
@@ -28,8 +28,10 @@ public class CityOperationsImpl implements CityOperations {
     @Override
     public int insertCity(@NotNull String name, String postalCode) {
            
-        String insertCityQuery = "INSERT INTO [dbo].[City] (Name, PostalCode) VALUES (?, ?); ";
-       
+        String insertCityQuery = "INSERT INTO [dbo].[City] " +
+                                "           (Name, PostalCode) " +
+                                "       VALUES (?, ?); ";
+
         try(PreparedStatement ps = connection.prepareStatement(insertCityQuery, Statement.RETURN_GENERATED_KEYS);) {           
             ps.setString(1, name);
             ps.setString(2, postalCode);
@@ -50,12 +52,39 @@ public class CityOperationsImpl implements CityOperations {
     
        
     @Override
-    public int deleteCity(String... strings) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public int deleteCity(String... names) {
+        
+        String deleteCityByNameQuery = "DELETE FROM [dbo].[City] " 
+                                    + " WHERE Name LIKE ?; ";
+        int numberOfDeletedCities = 0;
+        try(PreparedStatement ps = connection.prepareStatement(deleteCityByNameQuery);) {           
+            for(String cityName: names) {
+
+                ps.setString(1, cityName);
+                numberOfDeletedCities += ps.executeUpdate();
+                
+            }
+        } catch (SQLException ex) {
+//            Logger.getLogger(CityOperationsImpl.class.getName()).log(Level.SEVERE, null, ex);            
+        }
+        return numberOfDeletedCities;
+        
     }
 
     @Override
-    public boolean deleteCity(int i) {
+    public boolean deleteCity(int idCity) {
+
+        String deleteCityByIdQuery = "DELETE FROM [dbo].[City] " 
+                                    + " WHERE IdC = ?; ";
+
+        try(PreparedStatement ps = connection.prepareStatement(deleteCityByIdQuery);) {           
+            ps.setInt(1, idCity);
+
+            int numberOfDeletedCities = ps.executeUpdate();
+            return numberOfDeletedCities != 0;
+        } catch (SQLException ex) {
+//            Logger.getLogger(CityOperationsImpl.class.getName()).log(Level.SEVERE, null, ex);            
+        }
         return false;
     }
 
@@ -63,7 +92,8 @@ public class CityOperationsImpl implements CityOperations {
     public List<Integer> getAllCities() {
         List<Integer> listOfIds = new ArrayList<Integer>();
         
-        String getAllIdCQuery = "SELECT IdC FROM [dbo].[City]; ";
+        String getAllIdCQuery = "SELECT IdC "
+                                + " FROM [dbo].[City]; ";
        
         try(Statement stmt = connection.createStatement();
             ResultSet rs = stmt.executeQuery(getAllIdCQuery)){
@@ -93,20 +123,29 @@ public class CityOperationsImpl implements CityOperations {
         int vaId = cityOperations.insertCity("Valjevo", "14000");
         System.out.println(vaId); // 2
 
+        int nsId = cityOperations.insertCity("Novi Sad", "21000");    
+        System.out.println(nsId); // 3
+
         int vaBothSameId = cityOperations.insertCity("Valjevo", "14000");
         System.out.println(vaBothSameId); // -1
  
-        int vaSameNameId = cityOperations.insertCity("Valjevo_2", "14000");
-        System.out.println(vaSameNameId); // 3
-
-        int vaSamePostalCodeId = cityOperations.insertCity("Valjevo", "14011");
+        int vaSamePostalCodeId = cityOperations.insertCity("Valjevo_2", "14000");
         System.out.println(vaSamePostalCodeId); // -1
+
+        int vaSameNameId = cityOperations.insertCity("Valjevo", "14011");
+        System.out.println(vaSameNameId); // 4
         
         List<Integer> listOfIdC = cityOperations.getAllCities();
-        System.out.println(listOfIdC.size()); // 3
+        System.out.println(listOfIdC.size()); // 4
         for (int i: listOfIdC) {
             System.out.print(i + " ");
         }
+        System.out.println();
+        
+        System.out.println(cityOperations.deleteCity(vaSameNameId));
+        System.out.println(cityOperations.deleteCity(-1));
+        
+        System.out.println(cityOperations.deleteCity(new String[] {"Beograd", "Novi Sad", "Zrenjanin"}));
         
     }
     
