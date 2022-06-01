3154d6258bacbc36e4e2a96c0eaaa00dda8e441a
mdodovic <matija.dodovic@gmail.com>
2022-06-01 12:49:16 +0200

    Add Address table

---
+++ crateModel.sql
@@ -0,0 +1,44 @@
+
+DROP TABLE [Address]
+go
+
+DROP TABLE [City]
+go
+
+CREATE TABLE [Address]
+( 
+	[IdA]                integer  NOT NULL ,
+	[Street]             varchar(50)  NOT NULL ,
+	[Number]             integer  NOT NULL ,
+	[Xcoord]             integer  NOT NULL ,
+	[Ycoord]             integer  NOT NULL ,
+	[IdC]                integer  NOT NULL 
+)
+go
+
+CREATE TABLE [City]
+( 
+	[IdC]                integer  IDENTITY ( 1,1 )  NOT NULL ,
+	[Name]               varchar(50)  NOT NULL ,
+	[PostalCode]         varchar(20)  NOT NULL 
+)
+go
+
+ALTER TABLE [Address]
+	ADD CONSTRAINT [XPKAddress] PRIMARY KEY  CLUSTERED ([IdA] ASC)
+go
+
+ALTER TABLE [City]
+	ADD CONSTRAINT [XPKCity] PRIMARY KEY  CLUSTERED ([IdC] ASC)
+go
+
+ALTER TABLE [City]
+	ADD CONSTRAINT [XAK1City] UNIQUE ([PostalCode]  ASC)
+go
+
+
+ALTER TABLE [Address]
+	ADD CONSTRAINT [FKCity] FOREIGN KEY ([IdC]) REFERENCES [City]([IdC])
+		ON DELETE NO ACTION
+		ON UPDATE CASCADE
+go
