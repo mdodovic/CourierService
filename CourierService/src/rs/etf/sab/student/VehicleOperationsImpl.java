7d8e37e666f5bad97413d779fe621c52c2f8c091
mdodovic <matija.dodovic@gmail.com>
2022-06-02 19:03:15 +0200

    Insert Vehicle; Fetch all Vehicles

--- CourierService/src/rs/etf/sab/student/VehicleOperationsImpl.java
+++ CourierService/src/rs/etf/sab/student/VehicleOperationsImpl.java
@@ -5,8 +5,14 @@
  */
 package rs.etf.sab.student;
 
+import com.sun.istack.internal.NotNull;
 import java.math.BigDecimal;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 import rs.etf.sab.operations.VehicleOperations;
 
@@ -19,8 +25,27 @@ public class VehicleOperationsImpl implements VehicleOperations {
     private final Connection connection = DB.getInstance().getConnection();
 
     @Override
-    public boolean insertVehicle(String string, int i, BigDecimal bd, BigDecimal bd1) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public boolean insertVehicle(@NotNull String licencePlateNumber, int fuelType, BigDecimal fuelConsumtion, BigDecimal capacity) {
+
+        
+        String insertAddressQuery = "INSERT INTO [dbo].[Vehicle] " +
+                                    "       (LicencePlateNumber, FuelType, FuelConsumption, Capacity)" +
+                                    "   VALUES (?, ?, ?, ?) ";
+        
+        try(PreparedStatement ps = connection.prepareStatement(insertAddressQuery);) {           
+            ps.setString(1, licencePlateNumber);
+            ps.setInt(1, fuelType);
+            ps.setBigDecimal(3, fuelConsumtion);
+            ps.setBigDecimal(4, capacity);
+            
+            int numberOfInsertedUsers = ps.executeUpdate();
+            return numberOfInsertedUsers != 0;
+            
+        } catch (SQLException ex) {
+//            Logger.getLogger(CityOperationsImpl.class.getName()).log(Level.SEVERE, null, ex);            
+        }
+        return false;        
+
     }
 
     @Override
@@ -30,7 +55,24 @@ public class VehicleOperationsImpl implements VehicleOperations {
 
     @Override
     public List<String> getAllVehichles() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        List<String> listOfDrivingLicenceNumbers = new ArrayList<String>();
+        
+        String getAllUsernamesQuery = "SELECT DriverLicenceNumber "
+                                + " FROM [dbo].[Vehicle]; ";
+       
+        try(Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(getAllUsernamesQuery)){
+            
+            while(rs.next()){
+                listOfDrivingLicenceNumbers.add(rs.getString(1));
+            }
+            
+        } catch (SQLException ex) {
+//            Logger.getLogger(CityOperationsImpl.class.getName()).log(Level.SEVERE, null, ex);            
+        }
+
+        return listOfDrivingLicenceNumbers;
     }
 
     @Override
@@ -52,6 +94,15 @@ public class VehicleOperationsImpl implements VehicleOperations {
     public boolean parkVehicle(String string, int i) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+    
+    
+    public static void main(String[] args) {
+        VehicleOperations vehicleOperations = new VehicleOperationsImpl();
+        
+        System.out.println(vehicleOperations.insertVehicle("BG1675DA", 1, new BigDecimal(6.3D), new BigDecimal(100.5D)));
+        System.out.println(vehicleOperations.insertVehicle("BG1675DA", 1, new BigDecimal(6.3D), new BigDecimal(100.5D))); // same plate number
+        System.out.println(vehicleOperations.insertVehicle("VA119RT", 3, new BigDecimal(6.3D), new BigDecimal(100.5D))); // same plate number
 
+    }
     
 }
